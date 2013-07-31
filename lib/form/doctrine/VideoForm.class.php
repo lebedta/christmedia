5e82c644fffa49a6388c1ecfebadbe48dd7c203d
Lebed Tima <lebed.tima@gmail.com>
2013-08-01 02:09:06 +0300

    modified

--- lib/form/doctrine/VideoForm.class.php
+++ lib/form/doctrine/VideoForm.class.php
@@ -75,7 +75,7 @@ class VideoForm extends BaseVideoForm
 //        if($temp[1] == 'mp4')
 //        {
 //            $this->getObject()->setIsActive(true);
-//            $this->getObject()->setStatus('complete');
+//            $this->getObject()->setStatus('odidcomplete');
 //            $this->getObject()->setIsConverted(false);
 //            $this->getObject()->setDuration($total);
 //            $this->getObject()->save();
